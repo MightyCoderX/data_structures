10f129c662205df8f366ed9338ee30f4d4a9443f
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-05-05 23:25:37 +0200

    chore: update structure of each directory to for consistency

---
+++ linked_list/src/main.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    printf("Hello, World!\n");
+    return 0;
+}
