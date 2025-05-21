138ba57d7d0becd0da89b200bd33bfef01ce9f34
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-05-21 23:47:01 +0200

    feat: rename pointer param and complete basic functionality for int list

--- linked_list/src/linked_list/linked_list.c
+++ linked_list/src/linked_list/linked_list.c
@@ -7,69 +7,120 @@
     LLNodeType* new = malloc(sizeof(*new));                                                        \
     new->value = value;                                                                            \
     new->next = NULL;                                                                              \
-    if(p_linkedlist->tail != NULL) {                                                               \
-        ((LLNodeType*)p_linkedlist->tail)->next = new;                                             \
+    if(p_list->tail != NULL) {                                                                     \
+        ((LLNodeType*)p_list->tail)->next = new;                                                   \
     }                                                                                              \
-    if(p_linkedlist->head == NULL) {                                                               \
-        p_linkedlist->head = new;                                                                  \
+    if(p_list->head == NULL) {                                                                     \
+        p_list->head = new;                                                                        \
     }                                                                                              \
-    p_linkedlist->tail = new;                                                                      \
-    p_linkedlist->size += 1;
+    p_list->tail = new;                                                                            \
+    p_list->size++;
+
+#define linkedlist_insert(LLNodeType, index, value)                                                \
+    LLNodeType* new = malloc(sizeof(*new));                                                        \
+    new->value = value;                                                                            \
+    LLNodeType* prev = NULL;                                                                       \
+    LLNodeType* curr = p_list->head;                                                               \
+    size_t i = 0;                                                                                  \
+    while(curr != NULL && i < index && i < p_list->size) {                                         \
+        prev = curr;                                                                               \
+        curr = curr->next;                                                                         \
+        i++;                                                                                       \
+    }                                                                                              \
+    if(prev != NULL) {                                                                             \
+        prev->next = new;                                                                          \
+    } else {                                                                                       \
+        p_list->head = new;                                                                        \
+    }                                                                                              \
+    if(curr == NULL) {                                                                             \
+        p_list->tail = new;                                                                        \
+    }                                                                                              \
+    new->next = curr;                                                                              \
+    p_list->size++;
 
 LinkedList linkedlist_init() {
     LinkedList list = { NULL, NULL, 0 };
     return list;
 }
 
-void linkedlist_append_int(LinkedList* p_linkedlist, int value) {
+void linkedlist_append_int(LinkedList* p_list, int value) {
     linkedlist_append(LLNodeInt, value);
 }
 
-void linkedlist_append_float(LinkedList* p_linkedlist, float value) {
+void linkedlist_append_float(LinkedList* p_list, float value) {
     linkedlist_append(LLNodeFloat, value);
 }
-void linkedlist_append_double(LinkedList* p_linkedlist, double value) {
+void linkedlist_append_double(LinkedList* p_list, double value) {
     linkedlist_append(LLNodeDouble, value);
 }
-void linkedlist_append_generic(LinkedList* p_linkedlist, void* p_data) {
+void linkedlist_append_generic(LinkedList* p_list, void* p_data) {
     linkedlist_append(LLNodeGeneric, p_data);
 }
 
-void linkedlist_insert_int(LinkedList* p_linkedlist, size_t index, int value) {
-    LLNodeInt* new = malloc(sizeof(*new));
-    new->value = value;
+void linkedlist_insert_int(LinkedList* p_list, size_t index, int value) {
+    linkedlist_insert(LLNodeInt, index, value);
+}
+void linkedlist_insert_float(LinkedList* p_list, size_t index, float value) {
+    linkedlist_insert(LLNodeFloat, index, value);
+}
+void linkedlist_insert_double(LinkedList* p_list, size_t index, double value) {
+    linkedlist_insert(LLNodeDouble, index, value);
+}
 
-    LLNodeInt* prev = NULL;
-    LLNodeInt* curr = p_linkedlist->head;
+void linkedlist_insert_generic(LinkedList* p_list, size_t index, void* p_data) {
+    linkedlist_insert(LLNodeGeneric, index, p_data);
+}
+
+void linkedlist_remove_int(LinkedList* p_list, int value) {
+    if(p_list->head == NULL) return;
+
+    LLNodeInt pre_head = { .next = p_list->head };
+    LLNodeInt* prev = &pre_head;
+
+    while(prev->next != NULL) {
+        if(prev->next->value == value) {
+            break;
+        }
+
+        prev = prev->next;
+    }
+
+    LLNodeInt* target = prev->next;
+    if(target == p_list->head) {
+        p_list->head = target->next;
+    } else {
+        if(target == p_list->tail) {
+            p_list->tail = prev;
+        }
+        prev->next = target->next;
+    }
+    free(target);
+    p_list->size--;
+}
+void linkedlist_remove_float(LinkedList* p_list, float value);
+void linkedlist_remove_double(LinkedList* p_list, double value);
+void linkedlist_remove_generic(LinkedList* p_list, bool (*matches)(LLNodeGeneric* p_node));
+
+int linkedlist_get_int(LinkedList* p_list, size_t index) {
+    LLNodeInt* curr = p_list->head;
     size_t i = 0;
-    while(curr != NULL && i < index && i < p_linkedlist->size) {
-        prev = curr;
+    while(curr != NULL && i < index) {
         curr = curr->next;
         i++;
     }
 
-    if(prev != NULL) {
-        prev->next = new;
-    } else {
-        p_linkedlist->head = new;
-    }
-
-    if(curr == NULL) {
-        p_linkedlist->tail = new;
-    }
-
-    new->next = curr;
+    return curr->value;
 }
-void linkedlist_insert_float(LinkedList* p_linkedlist, size_t index, float value);
-void linkedlist_insert_double(LinkedList* p_linkedlist, size_t index, double value);
-void linkedlist_insert_generic(LinkedList* p_linkedlist, size_t index, void* data);
+float linkedlist_get_float(LinkedList* p_list, size_t index);
+double linkedlist_get_double(LinkedList* p_list, size_t index);
+void* linkedlist_get_generic(LinkedList* p_list, size_t index);
 
-void linkedlist_remove_int(LinkedList* p_linkedlist, int value);
-void linkedlist_remove_float(LinkedList* p_linkedlist, float value);
-void linkedlist_remove_double(LinkedList* p_linkedlist, double value);
-void linkedlist_remove_generic(LinkedList* p_linkedlist, bool (*matches)(LLNodeGeneric* p_node));
-
-int linkedlist_get_int(LinkedList* p_linkedlist, size_t index);
-float linkedlist_get_float(LinkedList* p_linkedlist, size_t index);
-double linkedlist_get_double(LinkedList* p_linkedlist, size_t index);
-void* linkedlist_get_generic(LinkedList* p_linkedlist, size_t index);
+void linkedlist_free_int(LinkedList* p_list) {
+    while(p_list->head != NULL) {
+        LLNodeInt* old = p_list->head;
+        p_list->head = old->next;
+        free(old);
+    }
+    p_list->tail = NULL;
+    p_list->size = 0;
+}
--- linked_list/src/main.c
+++ linked_list/src/main.c
@@ -1,9 +1,11 @@
+#include <assert.h>
 #include <stdio.h>
 
 #include "linked_list/linked_list.h"
 
 void print_ll_int(LinkedList* p_list) {
     LLNodeInt* curr = (LLNodeInt*)p_list->head;
+    printf("[%lu]: ", p_list->size);
     while(curr != NULL) {
         printf("%d->", ((LLNodeInt*)curr)->value);
         curr = curr->next;
@@ -11,15 +13,90 @@ void print_ll_int(LinkedList* p_list) {
     printf("NULL\n");
 }
 
+void validate_ll_int(LinkedList* p_list, int expected_head, int expected_tail) {
+    int head_val = ((LLNodeInt*)p_list->head)->value;
+    int tail_val = ((LLNodeInt*)p_list->tail)->value;
+    assert(head_val == expected_head);
+    assert(tail_val == expected_tail);
+
+    printf("head: %d, tail: %d\n", head_val, tail_val);
+}
+
+#ifdef DEBUG
+
+#endif /* ifdef DEBUG */
+
 int main(int argc, char** argv) {
+    /* 
+     * Init 
+     */
+    puts("\n/* Init */");
     LinkedList list = linkedlist_init();
+    print_ll_int(&list);
+
+    /*
+     * Append 
+     */
+    puts("\n/* Append */");
+
     linkedlist_append_int(&list, 1);
+    print_ll_int(&list);
+
     linkedlist_append_int(&list, 3);
+    print_ll_int(&list);
+
     linkedlist_append_int(&list, 4);
     print_ll_int(&list);
+
+    /*
+     * Insert
+     */
+    puts("\n/* Insert */");
 
     linkedlist_insert_int(&list, 1, 2);
     print_ll_int(&list);
 
+    linkedlist_insert_int(&list, 0, 0);
+    print_ll_int(&list);
+
+    linkedlist_insert_int(&list, list.size, 5);
+    print_ll_int(&list);
+
+    linkedlist_insert_int(&list, list.size + 100, 6);
+    print_ll_int(&list);
+
+    /*
+     * Get
+     */
+    puts("\n/* Get */");
+
+    printf("ll[0]: %d\n", linkedlist_get_int(&list, 0));
+    printf("ll[3]: %d\n", linkedlist_get_int(&list, 3));
+    printf("ll[6]: %d\n", linkedlist_get_int(&list, 6));
+
+    /*
+     * Remove
+     */
+    puts("\n/* Remove */");
+
+    validate_ll_int(&list, 0, 6);
+    linkedlist_remove_int(&list, 0);
+    validate_ll_int(&list, 1, 6);
+    print_ll_int(&list);
+
+    linkedlist_remove_int(&list, 3);
+    validate_ll_int(&list, 1, 6);
+    print_ll_int(&list);
+
+    linkedlist_remove_int(&list, 6);
+    validate_ll_int(&list, 1, 5);
+    print_ll_int(&list);
+
+    /*
+     * Free
+     */
+    puts("\n/* Free */");
+    linkedlist_free_int(&list);
+    print_ll_int(&list);
     return 0;
 }
